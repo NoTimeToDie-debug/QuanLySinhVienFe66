cfe2b73e05651c2a5f3d04fc3ba0e9f505dd528d
NoTimeToDie <nguyencatdat@gmail.com>
2021-06-14 21:04:45 +0700

    'lan_thu_n_roi_ne'

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const store = () =>{
     console.log('abc');
     return localStorage.getItem('store');
@@ -7,7 +6,3 @@ const store = () =>{
 function B() {
     console.log(' b edit');
 }
-=======
-const tuiCodeTuiCoQuyen = () => console.log('Đạt đẹp trai voãi');
-tuiCodeTuiCoQuyen();
->>>>>>> a6430bfebf0db571a4836c55ca2dfe1a82db06ef
